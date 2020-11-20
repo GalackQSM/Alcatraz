76cbb82a5f25099f365b97a831925b2a0b06dfda
GalackQSM <44106790+GalackQSM@users.noreply.github.com>
2020-11-20 16:56:33 +0000

    Mise a jour du help

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -14,21 +14,20 @@
 //=======================================================================                                                                      
                                                                        
 const Command = require('../Alcatraz.js');
+const ReactionMenu = require('../ReactionMenu.js');
 const { MessageEmbed } = require('discord.js');
-const emojis = require('../../utils/emojis.json');
 const { oneLine, stripIndent } = require('common-tags');
 const config = require('../../../config.json');
+const emojis = require('../../utils/emojis.json');
 
 module.exports = class HelpCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'help',
-      aliases: ['commands', 'h'],
-      usage: 'help [commandes | all]',
+      usage: 'help [commande]',
       description: oneLine`
         Affiche une liste de toutes les commandes actuelles, triées par catégorie.
         Peut être utilisé en conjonction avec une commande pour plus d'informations.
-        N'affichera que les commandes auxquelles vous êtes autorisé à accéder à moins que le paramètre \`all\` est donné.
       `,
       type: client.types.INFO,
       examples: ['help ping']
@@ -39,20 +38,13 @@ module.exports = class HelpCommand extends Command {
     let disabledCommands = message.client.db.settings.selectDisabledCommands.pluck().get(message.guild.id) || [];
     if (typeof(disabledCommands) === 'string') disabledCommands = disabledCommands.split(' ');
 
-    const all = (args[0] === 'all') ? args[0] : '';
-    const embed = new MessageEmbed();
     const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id); 
-    const { INFO, FUN, COLOR, POINTS, NFSW, GENERAL, JEUX, AVATAR, MOD, ADMIN, OWNER } = message.client.types;
     const { capitalize } = message.client.utils;
     
     const command = message.client.commands.get(args[0]) || message.client.aliases.get(args[0]);
-    if (
-      command && 
-      (command.type != OWNER || message.client.isOwner(message.member)) && 
-      !disabledCommands.includes(command.name)
-    ) {
+    if (command && command.type != message.client.types.OWNER && !disabledCommands.includes(command.name)) {
       
-      embed 
+      const embed = new MessageEmbed() 
         .setTitle(`Information de la commandes: \`${command.name}\``)
         .setThumbnail(message.guild.iconURL({ dynamic: true }))
         .setDescription(command.description)
@@ -64,8 +56,10 @@ module.exports = class HelpCommand extends Command {
       if (command.aliases) embed.addField('Aliases', command.aliases.map(c => `\`${c}\``).join(' '));
       if (command.examples) embed.addField('Exemples', command.examples.map(c => `\`${prefix}${c}\``).join('\n'));
 
-    } else if (args.length > 0 && !all) {
-      return this.sendErrorMessage(message, 0, 'Impossible de trouver la commande, veuillez vérifier la commande fournie');
+      message.channel.send(embed);
+
+    } else if (args.length > 0) {
+      return this.sendErrorMessage(message, `Impossible de trouver la commande \`${args[0]}\`. Veuillez saisir une commande valide.`);
 
     } else {
 
@@ -74,55 +68,105 @@ module.exports = class HelpCommand extends Command {
         commands[type] = [];
       }
 
-      const emojiMap = {
-        [INFO]: `${emojis.info} ${capitalize(INFO)}`,
-        [FUN]: `${emojis.fun} ${capitalize(FUN)}`,
-        [COLOR]: `${emojis.couleur} ${capitalize(COLOR)}`,
-        [POINTS]: `${emojis.points} ${capitalize(POINTS)}`,
-        [NFSW]: `${emojis.nsfw} ${capitalize(NFSW)}`,
-        [GENERAL]: `${emojis.general} ${capitalize(GENERAL)}`,
-        [JEUX]: `${emojis.jeux} ${capitalize(JEUX)}`,
-        [AVATAR]: `${emojis.avatar} ${capitalize(AVATAR)}`,
-        [MOD]: `${emojis.moderation} ${capitalize(MOD)}`,
-        [ADMIN]: `${emojis.admin} ${capitalize(ADMIN)}`,
-        [OWNER]: `${emojis.owner} ${capitalize(OWNER)}`
-      };
+      const { INFO, FUN, COLOR, POINTS, NFSW, GENERAL, JEUX, AVATAR, MOD, ADMIN, OWNER } = message.client.types;
+      const emojis = [
+        '<:alcatraz_afk:776109851159298082>',
+        '<:alcatraz_fun:776109856578338864>',
+        '<:alcatraz_color:776109859145252935>',
+        '<:alcatraz_points:776109859178807306>',
+        '<:alcatraz_nsfw:776109859010510868>',
+        '<:alcatraz_misc:776109858927411231>',
+        '<:alcatraz_emojis:776186958120091668>',
+        '<:alcatraz_pseudo:776109859568353321>',
+        '<:alcatraz_mod:776109858889924669>',
+        '<:alcatraz_admin:776109851175813170>',
+        '<:alcatraz_owner:776109858948251658>'
+      ];
 
       message.client.commands.forEach(command => {
-        if (!disabledCommands.includes(command.name)) {
-          if (command.userPermissions && command.userPermissions.every(p => message.member.hasPermission(p)) && !all)
-            commands[command.type].push(`\`${command.name}\`,`);
-          else if (!command.userPermissions || all) {
-            commands[command.type].push(`\`${command.name}\`,`);
-          }
-        }
+        if (!disabledCommands.includes(command.name)) commands[command.type].push(`\`${command.name}\`,`);
       });
 
-      const total = Object.values(commands).reduce((a, b) => a + b.length, 0) - commands[OWNER].length;
-      const size = message.client.commands.size - commands[OWNER].length;
-
-      embed 
+      const json = new MessageEmbed()
         .setTitle('Panel des commandes de Alcatraz')
         .setDescription(stripIndent`
           **Prefix:** \`${prefix}\`
           **Plus d'information:** \`${prefix}help [commande]\`
           **Nombre de commandes:** \`${message.client.commands.size}\`
-          ${(!all && size != total) ? `**Toutes les commandes:** \`${prefix}help all\`` : ''}
         `)
-        .setImage('https://i.imgur.com/WP9d2Z2.png')
+        .addField(`<:alcatraz_liens:776109858197209109> Liens`, '**[Ajouter Alcatraz](https://discordapp.com/oauth2/authorize?client_id=774652242787041310&scope=bot&permissions=2146958847) | '+'[Alcatraz Empire](https://discord.gg/aGP5kvxFEc) | '+'[Github](https://github.com/GalackQSM/Alcatraz) | '+'[SiteWeb](https://alcatraz-bot.com) | '+'[Dons](https://www.patreon.com/AlcatrazBot)**')
         .setFooter(config.footer)
         .setTimestamp()
         .setColor("#2f3136");
 
-      for (const type of Object.values(message.client.types)) {
-        if (type === OWNER && !message.client.isOwner(message.member)) continue;
-        if (commands[type][0])
-          embed.addField(`**${emojiMap[type]} [${commands[type].length}]**`, commands[type].join(' '));
-      }
+      const embed = new MessageEmbed() 
+        .setTitle('Commandes d\'Alcatraz')
+        .setThumbnail('')
+        .addField(`${emojis[0]} ${capitalize(INFO)}`, `\`${commands[INFO].length}\` commandes`, true)
+        .addField(`${emojis[1]} ${capitalize(FUN)}`, `\`${commands[FUN].length}\` commandes`, true)
+        .addField(`${emojis[2]} ${capitalize(COLOR)}`, `\`${commands[COLOR].length}\` commandes`, true)
+        .addField(`${emojis[3]} ${capitalize(POINTS)}`, `\`${commands[POINTS].length}\` commandes`, true)
+        .addField(`${emojis[4]} ${capitalize(NFSW)}`, `\`${commands[NFSW].length}\` commandes`, true)
+        .addField(`${emojis[5]} ${capitalize(GENERAL)}`, `\`${commands[GENERAL].length}\` commandes`, true)
+        .addField(`${emojis[6]} ${capitalize(JEUX)}`, `\`${commands[JEUX].length}\` commandes`, true)
+        .addField(`${emojis[7]} ${capitalize(AVATAR)}`, `\`${commands[AVATAR].length}\` commandes`, true)
+        .addField(`${emojis[8]} ${capitalize(MOD)}`, `\`${commands[MOD].length}\` commandes`, true)
+        .addField(`${emojis[9]} ${capitalize(ADMIN)}`, `\`${commands[ADMIN].length}\` commandes`, true)
+        .addField(`${emojis[10]} ${capitalize(OWNER)}`, `\`${commands[OWNER].length}\` commandes`, true)
+        .addField(`<:alcatraz_liens:776109858197209109> Liens`, '**[Ajouter Alcatraz](https://discordapp.com/oauth2/authorize?client_id=774652242787041310&scope=bot&permissions=2146958847) | '+'[Alcatraz Empire](https://discord.gg/aGP5kvxFEc) | '+'[Github](https://github.com/GalackQSM/Alcatraz) | '+'[SiteWeb](https://alcatraz-bot.com) | '+'[Dons](https://www.patreon.com/AlcatrazBot)**')
+        .setFooter(config.footer)
+        .setTimestamp()
+        .setImage('https://i.imgur.com/WP9d2Z2.png')
+        .setColor("#2f3136");
 
-      embed.addField(''+emojis.liens+' Liens', '**[Ajouter Alcatraz](https://discordapp.com/oauth2/authorize?client_id=774652242787041310&scope=bot&permissions=2146958847) | '+'[Alcatraz Empire](https://discord.gg/aGP5kvxFEc) | '+'[Github](https://github.com/GalackQSM/Alcatraz) | '+'[SiteWeb](https://alcatraz-bot.com) | '+'[Dons](https://www.patreon.com/AlcatrazBot)**');
-        
+      const reactions = {
+        '776109851159298082': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[0]} ${capitalize(INFO)}`, 
+          value: commands[INFO].join(' ')
+        }),
+        '776109856578338864': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[1]} ${capitalize(FUN)}`, 
+          value: commands[FUN].join(' ')
+        }),
+        '776109859145252935': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[2]} ${capitalize(COLOR)}`, 
+          value: commands[COLOR].join(' ')
+        }),
+        '776109859178807306': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[3]} ${capitalize(POINTS)}`, 
+          value: commands[POINTS].join(' ')
+        }),
+        '776109859010510868': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[4]} ${capitalize(NFSW)}`, 
+          value: commands[NFSW].join(' ')
+        }),
+        '776109858927411231': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[5]} ${capitalize(GENERAL)}`, 
+          value: commands[GENERAL].join(' ')
+        }),
+        '776186958120091668': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[6]} ${capitalize(JEUX)}`, 
+          value: commands[JEUX].join(' ')
+        }),
+        '776109859568353321': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[6]} ${capitalize(AVATAR)}`, 
+          value: commands[AVATAR].join(' ')
+        }),
+        '776109858889924669': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[6]} ${capitalize(MOD)}`, 
+          value: commands[MOD].join(' ')
+        }),
+        '776109851175813170': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[6]} ${capitalize(ADMIN)}`, 
+          value: commands[ADMIN].join(' ')
+        }),
+        '776109858948251658': new MessageEmbed(json).spliceFields(0, 0 , { 
+          name: `${emojis[6]} ${capitalize(OWNER)}`, 
+          value: commands[OWNER].join(' ')
+        }),
+      };
+
+      new ReactionMenu(message.channel, message.member, embed, reactions, 180000);
     }
-    message.channel.send(embed);
   }
 };
